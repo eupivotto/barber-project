db3df9a5f9059bd1fd12bee262b5c274207539e4
eupivotto <agenciadigi62@gmail.com>
2024-08-06 11:14:24 -0300

    feat: add header and input search on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
 
-
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
 
 const Home = () => {
-  return <Button>Teste</Button>
+  return (
+    <>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Lucas </h2>
+        <p>Sexta, 9 de Agosto.</p>
+
+        <div className="flex items-center gap-2 mt-6">
+        <Input placeholder="Faça sua busca..."/>
+        <Button size="icon">
+          <SearchIcon />
+        </Button>
+        </div>
+
+      </div>
+    </>
+  )
 }
 
 export default Home
