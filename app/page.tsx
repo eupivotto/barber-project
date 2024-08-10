7997eae53156a4b628da0cba74e4b8e6b0a4647b
eupivotto <agenciadigi62@gmail.com>
2024-08-09 23:49:08 -0300

    feat: add booking barbers

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import { SearchIcon } from "lucide-react"
 import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+
 
 const Home = () => {
   return (
@@ -18,6 +23,38 @@ const Home = () => {
           <SearchIcon />
         </Button>
         </div>
+
+        <div className="relative w-full h-[150px] ">
+        <Image 
+           alt="banner 01" 
+           src="/banner-01.png" 
+           fill 
+           className="object-cover rounded-xl" />
+        </div>
+        
+        
+          <Card className="mt-6">
+            <CardContent className="flex justify-between p-0">
+              <div className="flex flex-col gap-2 py-5 pl-5">
+                <Badge className="w-fit">Confirmado</Badge>
+                <h3 className="font-semibold">Corte de Cabelo</h3>
+                <div className="flex items-center gap-2">
+                  <Avatar className="h-6 w-6">
+                    <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
+                  </Avatar>
+                  <p className="text-sm">Belo Barbearia</p>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid"> 
+                <p className="text-sm">Agosto</p>
+                <p className="text-2xl">09</p>
+                <p className="text-sm">18:00</p>
+              </div>
+
+            </CardContent>
+          </Card>
+     
 
       </div>
     </>
